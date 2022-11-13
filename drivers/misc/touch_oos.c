9b7a0cb331640c178353b454a6beddd3965c0022
diphons <diphons@gmail.com>
2022-11-13 17:48:39 +0700

    Add support Oxygen Os 12
    OxygenOS 12 read double tap node in /proc/touchpanel/double_tap_enable
    add symlink from double_tap (SM8250) and gesture_enable (SDM845)
    
    maybe this patch can be useed on ColorOS 13

--- drivers/misc/touch_oos.c
+++ drivers/misc/touch_oos.c
@@ -14,7 +14,13 @@
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("diphons");
 MODULE_DESCRIPTION("oxygen os touch gesture");
-MODULE_VERSION("0.0.1");
+MODULE_VERSION("0.0.5");
+
+#define double_tap "touchpanel/double_tap_enable"
+#define genable "touchpanel/gesture_enable"
+#define tp_dt "touchpanel/double_tap"
+#define tp_g "touchpanel/gesture_enable"
+#define tpg "tp_gesture"
 
 static int __init d8g_touch_oos_init(void) {
     static struct proc_dir_entry *tp_oos;
@@ -22,7 +28,19 @@ static int __init d8g_touch_oos_init(void) {
 
 	printk(KERN_INFO "oxygen os touch gesture initial");
 
-	tp_oos = proc_symlink("tp_gesture", NULL, "touchpanel/gesture_enable");
+#ifdef CONFIG_BOARD_XIAOMI_SM8250
+	tp_oos = proc_symlink(double_tap, NULL, tp_dt);
+	if (!tp_oos)
+		ret = -ENOMEM;
+	tp_oos = proc_symlink(genable, NULL, tp_dt);
+	if (!tp_oos)
+		ret = -ENOMEM;
+#else
+	tp_oos = proc_symlink(double_tap, NULL, tp_g);
+	if (!tp_oos)
+		ret = -ENOMEM;
+#endif
+	tp_oos = proc_symlink(tpg, NULL, tp_g);
 	if (!tp_oos) {
 		ret = -ENOMEM;
 		printk(KERN_INFO "oxygen os touch gesture initial failed");
