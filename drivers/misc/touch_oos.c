d86ae0b050072f4630777910bcbe67c54780047d
diphons <diphons@gmail.com>
2022-11-14 07:40:59 +0700

    Add DT2W for focaltech also support SM8150
    Touchscreen FTS SDM845 not have symlink gesture_enable to use feature double tap on oxygen os..
    add double tap for focaltech touchsceen.. based on tp_common and symlink to /proc/touchpanel/gestrue_enable

--- drivers/misc/touch_oos.c
+++ drivers/misc/touch_oos.c
@@ -5,38 +5,97 @@
  * Copyright (C) 2022, Rudi Setiyawan <diphons@gmail.com>
  */
 
+#include <linux/interrupt.h>
 #include <linux/module.h>
 #include <linux/proc_fs.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
+#include <linux/fs.h>
 #include <linux/seq_file.h>
+#include <linux/irq.h>
+#include <linux/gpio.h>
+#ifdef CONFIG_ARCH_SDM845
+#include <linux/slab.h>
+#include <linux/input/tp_common.h>
+#endif
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("diphons");
 MODULE_DESCRIPTION("oxygen os touch gesture");
 MODULE_VERSION("0.0.5");
 
-#define double_tap "touchpanel/double_tap_enable"
-#define genable "touchpanel/gesture_enable"
-#define tp_dt "touchpanel/double_tap"
+#define tpdir "touchpanel"
+
+#define d_tap "touchpanel/double_tap_enable"
+#define tp_dt "/touchpanel/double_tap"
 #define tp_g "touchpanel/gesture_enable"
 #define tpg "tp_gesture"
+#define tpdir_fts "devices/platform/soc/a98000.i2c/i2c-3/3-0038/fts_gesture_mode"
+
+#ifdef CONFIG_ARCH_SDM845
+bool capacitive_keys_enabled;
+struct kobject *touchpanel_kobj;
+int cek_gesture = 0;
+module_param(cek_gesture, int, 0644);
+
+#define TS_ENABLE_FOPS(type)                                                   \
+	int tp_common_set_##type##_ops(struct tp_common_ops *ops)              \
+	{                                                                      \
+		static struct kobj_attribute kattr =                           \
+			__ATTR(type, (S_IWUSR | S_IRUGO), NULL, NULL);         \
+		kattr.show = ops->show;                                        \
+		kattr.store = ops->store;                                      \
+		return sysfs_create_file(touchpanel_kobj, &kattr.attr);        \
+	}
+
+TS_ENABLE_FOPS(capacitive_keys)
+TS_ENABLE_FOPS(double_tap)
+TS_ENABLE_FOPS(reversed_keys)
+#endif
 
 static int __init d8g_touch_oos_init(void) {
-    static struct proc_dir_entry *tp_oos;
+	char *driver_path;
+	static struct proc_dir_entry *tp_dir;
+	static struct proc_dir_entry *tp_oos;
 	int ret = 0;
 
 	printk(KERN_INFO "oxygen os touch gesture initial");
 
-#ifdef CONFIG_BOARD_XIAOMI_SM8250
-	tp_oos = proc_symlink(double_tap, NULL, tp_dt);
-	if (!tp_oos)
+#if defined(CONFIG_BOARD_XIAOMI_SM8250) || defined(CONFIG_MACH_XIAOMI_SM8250) || defined(CONFIG_ARCH_SM8150)
+	tp_dir = proc_mkdir(tpdir, NULL);
+	driver_path = kzalloc(PATH_MAX, GFP_KERNEL);
+	if (!driver_path) {
+		pr_err("%s: failed to allocate memory\n", __func__);
 		ret = -ENOMEM;
-	tp_oos = proc_symlink(genable, NULL, tp_dt);
+	}
+
+	sprintf(driver_path, "/sys%s", tp_dt);
+
+	pr_err("%s: driver_path=%s\n", __func__, driver_path);
+
+	tp_oos = proc_symlink(d_tap, NULL, driver_path);
+
+	if (!tp_oos) {
+		ret = -ENOMEM;
+	}
+	kfree(driver_path);
+	tp_oos = proc_symlink(tp_g, NULL, "double_tap_enable");
 	if (!tp_oos)
 		ret = -ENOMEM;
 #else
-	tp_oos = proc_symlink(double_tap, NULL, tp_g);
+	tp_dir = proc_mkdir(tpdir, NULL);
+	touchpanel_kobj = kobject_create_and_add("touchpanel", NULL);
+	if (!touchpanel_kobj)
+		ret = -ENOMEM;
+
+	sprintf(driver_path, "/sys%s", tp_dt);
+	pr_err("%s: driver_path=%s\n", __func__, driver_path);
+	tp_oos = proc_symlink(tp_g, NULL, driver_path);
+	if (!tp_oos) {
+		ret = -ENOMEM;
+	}
+	kfree(driver_path);
+	tp_oos = proc_symlink(d_tap, NULL, "gesture_enable");
 	if (!tp_oos)
 		ret = -ENOMEM;
 #endif
